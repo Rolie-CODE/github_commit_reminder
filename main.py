67a9f7eb70f52af4e5f4ddb893b833a5be9baf3b
Rolie-CODE <rytenkorang@st.ug.edu.gh>
2026-08-13 01:49:32 -0700

    fixed commit counting

--- main.py
+++ main.py
@@ -10,38 +10,43 @@ token = os.getenv("GITHUB_TOKEN")
 
 
 def get_today_commit_count():
-    url = f"https://api.github.com/users/{username}/events"
+
+    url = "https://api.github.com/search/commits"
+
+    today = datetime.now(timezone.utc).date()
+
+    query = f"author:{username} committer-date:{today}"
 
     headers = {
         "Authorization": f"Bearer {token}",
         "Accept": "application/vnd.github+json"
     }
 
-    response = requests.get(url, headers=headers)
+    params = {
+        "q": query,
+        "per_page": 100
+    }
+
+    response = requests.get(
+        url,
+        headers=headers,
+        params=params
+    )
 
     if response.status_code != 200:
-        print("Failed to get GitHub activity")
+        print("Failed to get commits")
+        print(response.text)
         return 0
 
-    events = response.json()
+    data = response.json()
 
-    today = datetime.now(timezone.utc).date()
-
-    commit_count = 0
-
-    for event in events:
-        if event["type"] == "PushEvent":
-            event_date = datetime.fromisoformat(
-                event["created_at"].replace("Z", "+00:00")
-            ).date()
-
-            if event_date == today:
-                commits = event["payload"].get("commits", [])
-                commit_count += len(commits)
+    commit_count = data.get("total_count", 0)
 
     return commit_count
 
+
 def send_telegram_message(message):
+
     bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
     chat_id = os.getenv("TELEGRAM_CHAT_ID")
 
@@ -56,13 +61,14 @@ def send_telegram_message(message):
 
     if response.status_code == 200:
         print("📱 Telegram reminder sent!")
+
     else:
         print("❌ Failed to send Telegram message")
         print(response.text)
 
 
-
 commit_count = get_today_commit_count()
+
 
 if commit_count > 0:
 
